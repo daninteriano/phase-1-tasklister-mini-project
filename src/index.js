870cc4c3dfcb723af5b72a4c947b8b5e159fbac2
Daniel Interiano <dannyboyint@gmail.com>
2021-10-13 18:10:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let myForm = document.querySelector('form')
+    myForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      newToDo = document.getElementById('new-task-description')
+      handlToDo(newToDo.value)
+      myForm.reset()
+    })
+    
+    // your code here
 });
+function handlToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x '
+  p.textContent = todo
+  p.appendChild(btn)
+  debugger;
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
